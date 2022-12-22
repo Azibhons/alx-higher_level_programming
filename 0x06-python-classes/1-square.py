c3e697e2b76cf21a35be35580cf5f3719097af08
Azibhons <nathanazibabhonomeni@gmail.com>
2022-12-22 03:02:28 +0100

    My project files update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Class Square that defines square based on 0-square.py 
+
+class Square:
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
